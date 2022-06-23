f6a18e7aa98178fe2eeac3e72a6f2f555d2f4706
Rahmatulloh <socialkavinsky@gmail.com>
2022-06-23 18:59:03 +0300

    Initial Commit

---
+++ SpotifyPlayerScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyPlayerScreen
+//
+//  Created by Rahmetullah on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
